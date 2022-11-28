a8620c077c789e7a4b5d7e4f80328e3a4b5f0f96
Mike Delmonaco <mthundershock@gmail.com>
2022-11-28 11:02:53 -0500

    provide stuff from main

--- main.rkt
+++ main.rkt
@@ -1,3 +1,18 @@
 #lang racket
 
-(provide)
+(provide dnumber?
+         dnumber->number
+         number->dnumber
+         derivative
+         +o
+         -o
+         *o
+         /o
+         for/sumo
+         for/producto
+         expo
+         expto
+         logo)
+
+(require "core.rkt"
+         "operators.rkt")
